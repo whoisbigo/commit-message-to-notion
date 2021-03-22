9bd9190c8caa077c20bb3ce69b9d605cdfc6cb16
BigO <bigo@BigOui-MacBookPro.local>
2021-03-22 18:03:24 +0900

    fix function call

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ pageUrl = os.environ['NOTION_PAGE_URL']
 print('getting page..(pageUrl: {})'.format(pageUrl))
 page = client.get_block(pageUrl, force_refresh=True)
 print('getting page done.(title:{})'.format(page.title))
-page.title = '{} - edited by github action'.format(page.title)
+# page.title = '{} - edited by github action'.format(page.title)
 
 # add test item
 # newchild = page.children.add_new(TodoBlock, title="Something to get done")
@@ -27,5 +27,5 @@ page.title = '{} - edited by github action'.format(page.title)
 commitMessage = os.environ['COMMIT_MESSAGE']
 print('adding item..(commitMessage: {})'.format(commitMessage))
 title = '{date} : {message}'
-page.children.add_new(BulletedListBlock, title=title.format(date=get_today_str, message=commitMessage))
+page.children.add_new(BulletedListBlock, title=title.format(date=get_today_str(), message=commitMessage))
 print('adding item done.')
