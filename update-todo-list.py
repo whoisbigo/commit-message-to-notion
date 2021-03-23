e0df54a3dbfa1c59ee64a247afc8b1e6b4af2999
BigO <bigo@BigOui-MacBookPro.local>
2021-03-23 17:26:27 +0900

    작업 기록 페이지 업데이트 스크립트 추가

---
+++ update-todo-list.py
@@ -0,0 +1,25 @@
+from notion.client import NotionClient
+from notion.block import *
+from notion.collection import *
+from datetime import datetime
+import notion
+import os
+
+print('creating client..')
+tokenV2 = os.environ['NOTION_TOKEN_V2']
+client = NotionClient(token_v2=tokenV2)
+print('creating client done.')
+
+pageUrl = os.environ['NOTION_PAGE_URL']
+print('getting page..(pageUrl: {})'.format(pageUrl))
+page = client.get_block(pageUrl, force_refresh=True)
+print('getting page done.(title:{})'.format(page.title))
+# page.title = '{} - edited by github action'.format(page.title)
+
+commitMessage = os.environ['COMMIT_MESSAGE']
+print('adding item..(commitMessage: {})'.format(commitMessage))
+#title = '{date} : {message}'
+title = '{message}'
+newchild = page.children.add_new(TodoBlock, title=title)
+newchild.checked = True
+print('adding item done.')
