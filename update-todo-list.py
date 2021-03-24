ab45745049341edc903c3c8bcf74e4d1a16ef5a8
BigO <bigo@BigOui-MacBookPro.local>
2021-03-24 11:17:15 +0900

    add creation parent node and insert todo item, add splitting message with comma

--- update-todo-list.py
+++ update-todo-list.py
@@ -5,6 +5,10 @@ from datetime import datetime
 import notion
 import os
 
+def get_today_str():
+  today = datetime.today()
+  return f'{today.year}-{today.month}-{today.day}' 
+
 print('creating client..')
 tokenV2 = os.environ['NOTION_TOKEN_V2']
 client = NotionClient(token_v2=tokenV2)
@@ -16,10 +20,28 @@ page = client.get_block(pageUrl, force_refresh=True)
 print('getting page done.(title:{})'.format(page.title))
 # page.title = '{} - edited by github action'.format(page.title)
 
+
+
+# find today's title
+titleToday = get_today_str()
+today = None
+for child in page.children:
+  if child.title == titleToday:
+    today = child
+    break
+
+if today == None:
+  today = page.children.add_new(ToggleBlock, title=titleToday)
+
+today.move_to(page, "first-child")
+
 commitMessage = os.environ['COMMIT_MESSAGE']
-print('adding item..(commitMessage: {})'.format(commitMessage))
-#title = '{date} : {message}'
-title = '{message}'
-newchild = page.children.add_new(TodoBlock, title=title.format(message=commitMessage))
-newchild.checked = True
-print('adding item done.')
+messageList = commitMessage.split(', ')
+
+for message in messageList:
+  print('adding item..(commitMessage: {})'.format(commitMessage))
+  #title = '{date} : {message}'
+  title = '{message}'
+  newchild = today.children.add_new(TodoBlock, title=title.format(message=commitMessage))
+  newchild.checked = True
+  print('adding item done.')
